d41d1215e198b5ebf34f6aa38013d6a7b4738e44
javikin <javikin10@gmail.com>
2025-02-05 11:44:56 -0500

    feat: added format to release date

--- lib/features/movies/presentation/pages/movie_details_page.dart
+++ lib/features/movies/presentation/pages/movie_details_page.dart
@@ -2,6 +2,7 @@ import 'package:cinemania/core/constants/constants.dart';
 import 'package:cinemania/core/widgets/cine_app_bar.dart';
 import 'package:cinemania/features/movies/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class MovieDetailsPage extends StatelessWidget {
   final Movie movie;
@@ -10,6 +11,11 @@ class MovieDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final locale = Localizations.localeOf(context).toLanguageTag();
+    final releaseDate = DateTime.tryParse(movie.releaseDate);
+
+    final formattedDate = releaseDate != null ? DateFormat.yMMMd(locale).format(releaseDate) : movie.releaseDate;
+
     return Scaffold(
       appBar: CineAppBar(title: movie.title),
       body: SingleChildScrollView(
@@ -29,11 +35,17 @@ class MovieDetailsPage extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(movie.title),
+                  Text(
+                    movie.title,
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   const SizedBox(height: 8),
                   Text(movie.genreNames.join(', ')),
                   const SizedBox(height: 8),
-                  Text(movie.releaseDate),
+                  Text(formattedDate),
                   const SizedBox(height: 8),
                   Text(movie.overview),
                 ],
